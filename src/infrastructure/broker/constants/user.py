6486ad6766b008b810ab01b8e96cf5b17d9e51c2
Maxim <yrev2011@gmail.com>
2025-01-18 16:31:42 +0700

    Edit queue name

--- src/infrastructure/broker/constants/user.py
+++ src/infrastructure/broker/constants/user.py
@@ -1,4 +1,4 @@
 from faststream.rabbit import RabbitExchange, RabbitQueue
 
 USER_EXCHANGE = RabbitExchange("user")
-USER_INFO_QUEUE = RabbitQueue("user-info")
+USER_INFO_QUEUE = RabbitQueue("user-info-create")
